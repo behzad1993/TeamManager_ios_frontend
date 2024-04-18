7f766415b7f1d78027961ec49daef303c3e34e1d
Behzad Karimi <karimi1993@live.de>
2024-04-18 02:15:45 +0200

    Initial Commit

---
+++ TeamManager_ios/TeamManager_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamManager_iosApp.swift
+//  TeamManager_ios
+//
+//  Created by Behzad Karimi on 18.04.24.
+//
+
+import SwiftUI
+
+@main
+struct TeamManager_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
